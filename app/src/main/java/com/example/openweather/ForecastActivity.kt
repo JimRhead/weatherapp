9dc701f033a9e1c3e23d18d4286e5ebbda56847f
James Rhead <james.rhead@and.digital>
2020-10-21 10:14:54 +0100

    Code is working. onCreate was incorrect

--- app/src/main/java/com/example/openweather/ForecastActivity.kt
+++ app/src/main/java/com/example/openweather/ForecastActivity.kt
@@ -8,8 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.forcast_activity.*
 
 class ForecastActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.forcast_activity)
         val cityName = intent.getStringExtra(EXTRA_MESSAGE)
         forecastCityName.text = cityName
