8415bd2ed458e9196fdce91738feca95c7d45cc5
James Rhead <james.rhead@and.digital>
2020-10-21 09:59:45 +0100

    Broken code for review

---
+++ app/src/main/java/com/example/openweather/ForecastActivity.kt
@@ -0,0 +1,33 @@
+package com.example.openweather
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.forcast_activity.*
+
+class ForecastActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.forcast_activity)
+        val cityName = intent.getStringExtra(EXTRA_MESSAGE)
+        forecastCityName.text = cityName
+    }
+
+    companion object{
+        @JvmStatic
+        fun newInstance(context: Context?, cityName: String): Intent {
+            return Intent(context, ForecastActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, cityName)
+            }
+        }
+
+        private const val EXTRA_MESSAGE = "CITY NAME"
+
+        @JvmField
+        val TAG = ForecastActivity::class.java.simpleName
+    }
+
+
+}
--- app/src/main/java/com/example/openweather/MainActivity.kt
+++ app/src/main/java/com/example/openweather/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.openweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkWeatherButton.setOnClickListener{getForecastforCity()}
+    }
+
+    private fun getForecastforCity(){
+        val cityName = cityName.text.toString()
+        startActivity(ForecastActivity.newInstance(this, cityName))
     }
 }
